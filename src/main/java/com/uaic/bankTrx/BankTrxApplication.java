0a7bef0c6f5289234e85012b5fc1027062aa2940
raresChelariu <rareschelariu79@hotmail>
2019-03-09 14:51:13 +0200

    first commit

---
+++ src/main/java/com/uaic/bankTrx/BankTrxApplication.java
@@ -0,0 +1,13 @@
+package com.uaic.bankTrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTrxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTrxApplication.class, args);
+	}
+
+}
